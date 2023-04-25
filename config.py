2a8897bb1ace7d6d95ba0bbc5311289b2e9eaeb0
Mooncess <shulik.2003@mail.ru>
2023-04-25 21:22:05 +0300

    new config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 TOKEN="5928518957:AAFAoH4G5Jtow6QC3LZ4ZZ3uZdrXrYH1ZX8"
-host="127.0.0.1"
+host="postgres_db" # Для Docker-контейнеров
+#host="127.0.0.1" # Для локальных машин
 user="postgres"
-password="z26200307eD1"
+password="password"
 db_name="DnDBot"
 port="5432"
