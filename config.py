34b5398f16a1dbcd8f1b977b11e29034b4cb5db9
Vladimir Dorofeev <90868048+Necromemeser@users.noreply.github.com>
2023-05-17 20:50:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TOKEN=""
 host="postgres_db" # Для Docker-контейнеров
 #host="127.0.0.1" # Для локальных машин
-user="postgres"
-password="password"
-db_name="DnDBot"
-port="5432"
+user=""
+password=""
+db_name=""
+port=""
